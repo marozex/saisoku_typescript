97e148a8905e300a87e3521bc575d073edbc7988
shimakura <shimakura0213@gmail.com>
2020-11-28 18:38:47 +0900

    feat: 型エイリアス宣言、TestComponent追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import TestComponent from './TestComponent'
 
 let username: string = 'Hello'
 
@@ -8,22 +9,11 @@ let dummy: number = 1
 
 const func1 = (x: number, y: number) => x + y
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TestComponent text="hogehoge" />
       </header>
     </div>
   )
