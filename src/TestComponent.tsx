e2bb6d15e489ec1f33a7745a7cc79737f58c16f1
shimakura <shimakura0213@gmail.com>
2020-11-28 18:50:36 +0900

    feat: useState記述

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 type Props = {
   text: string
 }
+type UserData = {
+  id: number
+  name: string
+}
 
 const TestComponent: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<number | null>(0)
+  const [user, setUser] = useState<UserData>({ id: 1, name: 'tes' })
+
   return (
     <div>
       <h1>{props.text}</h1>
+      <h1>{count}</h1>
+      <h1>
+        {user.id}
+        {user.name}
+      </h1>
     </div>
   )
 }
